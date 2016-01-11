492d30ad0774e757f666ca1c84d7d38e6491cdda
unknown <atrijffel@PCD3169.d1.dsw.lan>
2016-01-11 19:08:31 +0100

    prerequisites script

---
+++ install_prerequisites.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Download and install Nodejs for windows from https://nodejs.org/en/#download
+start https://nodejs.org/en/#download
+echo Press a key when the installation of Nodejs has completed.
+pause
+cd /d "%~dp0\src"
