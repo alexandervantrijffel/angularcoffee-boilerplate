ccaf3d13860d3eb661ce3d201a603c0ba6a4f094
unknown <atrijffel@PCD3169.d1.dsw.lan>
2016-01-12 11:57:08 +0100

    Added installation of Gulp and Bower

--- install_prerequisites.bat
+++ install_prerequisites.bat
@@ -2,6 +2,10 @@
 echo Download and install Nodejs for windows from https://nodejs.org/en/#download
 start https://nodejs.org/en/#download
 echo Press a key when the installation of Nodejs has completed.
-pause
 cd /d "%~dp0\src"
-"C:\Program Files\nodejs\npm" install
+echo Installing Gulp globally
+start "" "C:\Program Files\nodejs\npm" install -g gulp
+echo Installing Bower globally
+"C:\Program Files\nodejs\npm" install -g bower
+cd /d "%~dp0"
+echo Run start_server.cmd to continue...
